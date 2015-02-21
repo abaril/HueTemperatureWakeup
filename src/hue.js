d407b4e97eb98b3875805c2d4da95e1f6ff92bc6
Allan Baril <allan.baril@gmail.com>
2015-02-21 17:31:20 -0500

    @abaril continuing

--- src/hue.js
+++ src/hue.js
@@ -57,13 +57,13 @@ function start(settings, controller) {
 
 	forecastRequestURL = createForecastRequestURL(settings);
 
-   lightController = controller; 
+    lightController = controller; 
 
 	light_id = settings.light_id;
-	api = new hue.HueApi(settings.gateway_address, settings.gateway_user);
-	api.connect().then(displayResult).done();   
+	// api = new hue.HueApi(settings.gateway_address, settings.gateway_user);
+	// api.connect().then(displayResult).done();   
    
-   setTimeout(turnLightOff, 10000);
+ //    setTimeout(turnLightOff, 10000);
 }
 
 function triggerAlarm() {
